3b7794343b43630a7e6e3b9310a09a8a40c90625
Navinofc44 <Pahasarasanujja@gmail.com>
2024-10-05 18:12:51 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "put session_id",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://8030.us.kg/file/zOm4HoO6YnQR.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello 👋 🇱🇰 *I AM ONLINE NOW ! Queen Chooty Nelumi Md* 🇱🇰",
+SUDO_NB: process.env.SUDO_NB || "94757286833",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public"
+};
+
